0f9bcd97bfedb679b269d0328a57849cf98386a3
Kim Quibral <kimquibral2000@gmail.com>
2023-01-21 15:39:15 +0800

    comment ip

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const ip = require('ip')
+// const ip = require('ip')
+// const IP = ip.address()
+// const IP_AND_PORT = `SERVER IS ONLINE ON\n${IP}:${PORT}`
 
 const app = express()
 const connection = mongoose.connection
@@ -27,9 +29,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/users', userRoute)
 
 const PORT = process.env.PORT || 2023
-const IP = ip.address()
+const PORT_ONLY = `SERVER IS ONLINE ON PORT: ${PORT}`
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`SERVER IS ONLINE ON\n${IP}:${PORT}`)
+    console.log(PORT_ONLY)
     console.log('------------------------------')
     console.log('CONNECTING TO DATABASE...')
 })
